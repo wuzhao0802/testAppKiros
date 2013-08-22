82f51f0ae99d3a23acc9324de757ddae5484d7c4
xw80545 <xw80545@APACCNSHZJW2037.apac.nsroot.net>
2013-08-22 12:45:54 +0800

    add the service

---
+++ src/com/citi/service/GitService.java
@@ -0,0 +1,5 @@
+package com.citi.service;
+
+public interface GitService {
+
+}
---
+++ src/com/citi/service/impl/gitSerivceImpl.java
@@ -0,0 +1,7 @@
+package com.citi.service.impl;
+
+import com.citi.service.GitService;
+
+public class gitSerivceImpl implements GitService{
+
+}
